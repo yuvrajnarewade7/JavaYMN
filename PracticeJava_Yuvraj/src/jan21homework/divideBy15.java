8339658b7db78127f62ecd9742e0c6533e14e55e
YuvrajNarewade <YuvrajNarewade@LAPTOP-VOST8HAC>
2024-01-27 11:01:59 +0530

    update to divideby15

--- PracticeJava_Yuvraj/src/jan21homework/divideBy15.java
+++ PracticeJava_Yuvraj/src/jan21homework/divideBy15.java
@@ -6,15 +6,15 @@ public class divideBy15 {
 		// TODO Auto-generated method stub
 		try {
 			double temp = Math.random();
-			System.out.println(temp);	
-			System.out.println(15/0);	
+			System.out.println(temp);
+			System.out.println(15/temp);	
 		}catch(Throwable t) {
 		
 			System.out.println("Exception occured"+ t.getMessage());
 			t.printStackTrace();
 		}
 		
-		
+				
 	}
 
 }
