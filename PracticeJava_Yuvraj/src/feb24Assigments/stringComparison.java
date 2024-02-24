5371a335f4e6b177bf136e5164a3b76c6df8c49f
YuvrajNarewade <YuvrajNarewade@LAPTOP-VOST8HAC>
2024-02-24 17:16:33 +0530

    string comparison

---
+++ PracticeJava_Yuvraj/src/feb24Assigments/stringComparison.java
@@ -0,0 +1,17 @@
+package feb24Assigments;
+
+public class stringComparison {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String str1 = "abcdef";
+		String str2 = "abcde";
+		
+		if (str1 == str2) 
+			System.out.println("Strings are equal");
+		else 
+			System.out.println("Strings are not equal");
+	}
+
+}
